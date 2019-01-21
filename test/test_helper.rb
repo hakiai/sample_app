93ac04ad9bbf0a21df5975e4a4fb7410e55af98a
hakiai <chu_noge0822@yahoo.co.jp>
2019-01-21 10:54:44 +0900

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
 
@@ -10,4 +10,7 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
